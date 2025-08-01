754265d1a5ab68bbca3fb25c68b622c2a4eccc44
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-02 01:26:27 +0600

    isArray

---
+++ isArray.js
@@ -0,0 +1,13 @@
+const friends = ["dan", "ann", "kane", "mann", "kan", "han"];
+const num = [];
+const food = "ros o gol";
+const age = 15;
+
+console.log(Array.isArray(friends));
+// output is true
+console.log(Array.isArray(num));
+// output is true
+console.log(Array.isArray(food));
+// output is false
+console.log(Array.isArray(age));
+// output is false
