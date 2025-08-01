a600954c03d36e4f1deee454a12e4eca05ffe002
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-02 02:21:29 +0600

    slice

--- slice.js
+++ slice.js
@@ -1,5 +1,11 @@
+const a = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// slicing on an array
+console.log(a.slice(1, 5));
+// output [ 2, 3, 4, 5 ]
+// array.slice(1,5) --> elements will be taken starting from index 1 to upto index 5, but not including index 5.
 
 
-
-
-// array.slice(1,5) elements will be taken starting from index 1 to upto index 5, but not including index 5.
+console.log(a.slice(3));
+// output [ 4, 5, 6, 7, 8 ]
+// array.slice(3) --> starts removing elements from index 3
--- splice.js
+++ splice.js
@@ -1,7 +1,6 @@
-
-
-
+const a = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(a.splice(1, 5));
 // array.splice(5,2) delete count is 2, so two elements will be removed starting from index 5.
 
-
+console.log(a.splice(3));
 // array.splice(2). starts removing elements from index 2.
