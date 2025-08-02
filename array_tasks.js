e55cc1ddc06fd38e6f35937f39640e6513a7bef0
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-03 00:00:26 +0600

    tasks done

--- array_tasks.js
+++ array_tasks.js
@@ -17,8 +17,11 @@ console.log(fruits);
 // d.Remove the last tourist destination you have added
 // e.display the final array as output
 
-
-
+const destination = ["cox's", "dhaka", "chat"];
+destination.push("newark");
+destination.push("rome", "los");
+destination.pop();
+console.log(destination);
 
 // 3. Checking Array Membership with ‘includes’
 // Instructions:
@@ -29,6 +32,14 @@ console.log(fruits);
 
 // c.Print a message to the console indicating whether the element is present in the array or not.
 
+const book = ["book1", "book2", "book3", "book4", "book5"];
+console.log(book.includes("book1"));
+if (book.includes("book1")) {
+  console.log("book1 is present in the array");
+} else {
+  console.log("book1 is not present in the array");
+}
+
 // 4. Checking if it's an Array
 // Instructions:
 
@@ -38,12 +49,34 @@ console.log(fruits);
 
 // c.Print a message to the console indicating whether each variable is an array or not.
 
+const num = (5, 6, 7, 8, 9);
+const name = ["ami", "tum", "se"];
+const is = (true, false, true, false);
+if (Array.isArray(num)) {
+  console.log("num is an array");
+} else {
+  console.log("num is not an array");
+}
+if (Array.isArray(name)) {
+  console.log("name is an array");
+} else {
+  console.log("name is not an array");
+}
+if (Array.isArray(is)) {
+  console.log(" is is an array");
+} else {
+  console.log("is is not an array");
+}
+
 // 5. Combining Arrays
 // Instructions:
 
 // a.Create two arrays of your choice.
 // b.Use the concat method to combine the two arrays into a new array.
 // c.Print both the original arrays and the combined array using console.log().
-//  *
-//  *
-//  * **/
+
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+console.log(a);
+console.log(b);
+console.log(a.concat(b));
